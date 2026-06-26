5cff1506d7e2ffa903172b511c075745c5ed6a6a
prajwal <prajwalk2200@gmail.com>
2026-06-26 12:35:53 +0530

    Added two files in main

---
+++ merge/emp.java
@@ -0,0 +1,12 @@
+
+public class Car {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Git and GitHub");
+
+    }
+
+}
